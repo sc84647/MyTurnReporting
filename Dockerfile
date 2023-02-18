d4655a28fdd5b68b1c9aa97d13aa0731d467cec9
steve <stephen.cook.email@gmail.com>
2023-02-18 12:10:44 +0000

    Version 2 - using Google Sheets

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY main.py .
+COPY *.json .
+
+RUN apt-get update && apt-get install -y cron dos2unix
+COPY crontab /etc/cron.d/crontab
+RUN chmod 0644 /etc/cron.d/crontab
+RUN dos2unix /etc/cron.d/crontab
+RUN /usr/bin/crontab /etc/cron.d/crontab
+
+#CMD [ "python", "./main.py", "--persist" ]
+CMD [ "cron", "-f" ]
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import requests
 import json
 import urllib.parse
-import subprocess
 import os
 import sys
 import argparse
+import logging
+
+import gspread
+import csv
+
+def update_gsheet(dataset):
+    logging.info("%s", {key: dataset[key] for key in dataset.keys() & {'gsheet_id', 'gdrive_name', 'gsheet_worksheet_name'}})
+    
+    csvFile = dataset['filename']
+    csvReader = csv.reader(open(csvFile))
+    csvData = list(csvReader)
+
+    gc = gspread.service_account(filename='google_serviceaccount_credentials.json')
+    sh = gc.open_by_key(dataset['gsheet_id'])
+    wsh = sh.worksheet(dataset['gsheet_worksheet_name'])
+    wsh.clear()
+
+    sh.values_update(dataset['gsheet_worksheet_name'],
+                     params={'valueInputOption': 'USER_ENTERED'},
+                     body={'values': csvData})
+
+    logging.info("Read %s lines from file, parsed as %s rows.  Sheet now has %s rows.", 
+                 csvReader.line_num, len(csvData), wsh.row_count)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--datasets', required=False, nargs='?', default='datasets.json')
@@ -13,17 +35,25 @@ parser.add_argument('--no-persist', dest='persist', action='store_false')
 parser.set_defaults(persist=False)
 args = parser.parse_args()
 
+## Setup the logging
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+log_format = logging.Formatter('[%(asctime)s] [%(levelname)s] - %(message)s')
+handler = logging.StreamHandler(sys.stdout)                             
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(log_format)                                        
+root.addHandler(handler)
+
 ## Load the global configurations
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'configuration.json')
 f3 = open(filename,)
 configs = json.load(f3)
 
-filename = os.path.join(dirname, 'credentials.json')
+filename = os.path.join(dirname, 'myturn_credentials.json')
 f = open(filename,)
 creds = json.load(f)
 
-## TODO - form this URL from configs['myturn-server']
 URL_LOGIN = 'https://' + configs['myturn-server'] + configs['myturn-login-path']
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'
 
@@ -44,8 +74,7 @@ datasets = json.load(f2)
 for dataset in datasets:
     filename = dataset['filename']
     url = 'https://' + configs['myturn-server'] + dataset['urlpath']
-    gcs_object = 'gs://' + configs['gs-bucket-name'] + '/' + dataset['gcs-object']
-    print(filename, gcs_object)
+    logging.info("Fetching from MyTurn: %s", filename)
 
     response = s.request("GET", url)
     response.raise_for_status() # ensure we notice bad responses
@@ -53,4 +82,4 @@ for dataset in datasets:
         f_out.write(response.content.decode('utf-8'))
         f_out.close()
     if args.persist:
-        result = subprocess.run(["gsutil", "cp", filename, gcs_object])
+        update_gsheet(dataset)
